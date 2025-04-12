a6ec956c33ee1446f873d6c6510c94f9e4a14404
shawtyy25 <szolnokiabi25@gmail.com>
2025-04-12 11:51:50 +0200

    base with database connection

--- server/server.js
+++ server/server.js
@@ -1,21 +1,48 @@
 import express from "express"
 import path from "path"
-import { fileURLToPath } from "url"
+import cors from "cors"
+import pkg from "pg"
+import dotenv from "dotenv"
 
-const _filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
+dotenv.config({path: './client.env'});
+
+const _dirname = path.resolve();
 
 const app = express();
-const PORT = process.env.PORT || 3300;
+const port = process.env.PORT || 3300;
+
+app.use(cors());
+app.use(express.json());
+
+const { Client } = pkg;
+
+const client = new Client({
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: `${process.env.DB_PASSWORD}`,
+    port: process.env.DB_PORT,
+});
+
+async function connectToDB() {
+    try {
+        await client.connect();
+        console.log('Successfully connected to database!');
+    } catch (e) {
+        console.error('Error connecting to database!', e);
+        process.exit(1);
+    }
+}
+
+connectToDB();
 
 const angularPath = path.join(_dirname, '../ecommerce/src');
-
 app.use(express.static(angularPath));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(angularPath, 'index.html'));
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 })

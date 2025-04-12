0c681ff141fdd3dfb308c3bb2a982acd4dd2a2f0
shawtyy25 <szolnokiabi25@gmail.com>
2025-04-12 15:15:09 +0200

    show | hide password + BUILT

--- ecommerce/src/app/app.routes.ts
+++ ecommerce/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {RegistrationComponent} from './registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./login/login.component').then(m => m.LoginComponent);
+    }
+  },
+
+  {
+    path: 'registration',
+    loadComponent: () => {
+      return import('./registration/registration.component').then(m => m.RegistrationComponent);
+    }
+  },
+
+];
--- ecommerce/src/app/login/login.component.ts
+++ ecommerce/src/app/login/login.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    RouterLink,
+    NgClass
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  passVisibilityState: boolean =  false;
+  inputType: string = 'password';
 
+
+  toggleInputType(): void {
+    this.passVisibilityState = !this.passVisibilityState;
+    this.inputType = this.passVisibilityState ? 'text' : 'password';
+  }
 }

a4de9eafdc4bc0da940a15909e1c9944a109dc20
shawtyy25 <szolnokiabi25@gmail.com>
2025-10-17 18:22:07 +0200

    POPUP pricing inputs + date(TODO)

--- ecommerce/src/app/beta/admin-card-template/new-product-popup-template/new-product-popup-template.component.ts
+++ ecommerce/src/app/beta/admin-card-template/new-product-popup-template/new-product-popup-template.component.ts
@@ -1,9 +1,12 @@
-import {Component, signal} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DialogService} from '../../../services/admin/dialog.service';
 import {Dialog} from 'primeng/dialog';
 import {FloatLabel} from 'primeng/floatlabel';
 import {InputText} from 'primeng/inputtext';
 import {FormsModule} from '@angular/forms';
+import {Textarea} from 'primeng/textarea';
+import {InputNumber} from 'primeng/inputnumber';
+import {DatePicker} from 'primeng/datepicker';
 
 @Component({
   selector: 'app-new-product-popup-template',
@@ -12,6 +15,9 @@ import {FormsModule} from '@angular/forms';
     FloatLabel,
     InputText,
     FormsModule,
+    Textarea,
+    InputNumber,
+    DatePicker,
   ],
   templateUrl: './new-product-popup-template.component.html',
   styleUrl: './new-product-popup-template.component.scss'
@@ -25,5 +31,10 @@ export class NewProductPopupTemplateComponent {
   visible: boolean = false;
 
   productName: string = '';
+  productDesc: string = '';
+  price: number | undefined;
+  price_valid_from: Date | undefined;
+  price_valid_to: Date | undefined;
+  readonly huDateFormat: string = 'yy/mm/dd';
 
 }

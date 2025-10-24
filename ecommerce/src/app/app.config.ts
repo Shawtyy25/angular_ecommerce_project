2280d1f179f59ec3c7bf9d73113b510c74301859
xls-ubuntu <szolnokiabi25@gmail.com>
2025-10-24 21:42:42 +0200

    PORTABLE

--- ecommerce/src/app/app.config.ts
+++ ecommerce/src/app/app.config.ts
@@ -1,31 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import {provideHttpClient} from '@angular/common/http';
-import { TreeModule } from 'primeng/tree';
-import {providePrimeNG} from 'primeng/config';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import Aura from "@primeuix/themes/Aura";
-import {definePreset} from '@primeuix/themes';
-
-const auraLightPreset = definePreset(Aura, {
-  semantic: {
-    colorScheme: {
-      light: {
-        primary: 'indigo',
-        accent: 'lime',
-        surface: 'indigo',
-        text: 'gray.900',
-      },
-      dark: {
-        primary: 'indigo',
-        accent: 'lime',
-        surface: 'indigo',
-        text: 'gray.100',
-      },
-    },
-  },
-});
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from "@primeuix/themes/aura"
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -35,7 +14,7 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
-        preset: auraLightPreset
+        preset: Aura
       }
     })
   ]
